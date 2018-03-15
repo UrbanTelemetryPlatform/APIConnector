53e6ca9415cda7f44c3fba4bba23887cb722e205
Michael Dietz <michael.dtz@gmail.com>
2018-03-15 22:14:13 +0100

    update
    Signed-off-by: Michael Dietz <michael.dtz@gmail.com>

--- service.go
+++ service.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"cloud.google.com/go/pubsub"
 	"google.golang.org/appengine"
+	"google.golang.org/appengine/urlfetch"
 )
 
 type output struct {
@@ -15,20 +18,67 @@ type output struct {
 }
 
 func main() {
-	router := mux.NewRouter()
-
-	//Register api paths
-	router.Path("/api/read").Methods("GET").HandlerFunc(readAPI)
-	router.Path("/api/auth").Methods("GET").HandlerFunc(readAPI)
-
-	//Startup
-	http.Handle("/", router)
-	fmt.Println("server is starting up now")
+	http.HandleFunc("/api/auth", authCallback)
+	http.HandleFunc("/api/read", readAPI)
 	appengine.Main()
 }
 
 func readAPI(w http.ResponseWriter, r *http.Request) {
 
+	token := "MUPHbw0OutH5Hnp0UT6lgM5rc"
+	apiurl := "https://data.cityofchicago.org/resource/8v9j-bter.json"
+	projectID := "utp-md"
+	topicName := "traffic_update"
+
+	ctx := appengine.NewContext(r)
+	client := urlfetch.Client(ctx)
+
+	//Execute GET
+	resp, err := client.Get(apiurl + "?$$app_token=" + token + "&$limit=1400")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// Get body
+	response, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var dataInResponse []interface{}
+	err = json.Unmarshal([]byte(response), &dataInResponse)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "Response from API with %v entries\n", len(dataInResponse))
+
+	// Creates a pubsub client.
+	psClient, err := pubsub.NewClient(ctx, projectID)
+	if err != nil {
+		http.Error(w, "Failed to create pubsub client "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	messageCnt := 0
+
+	topic := psClient.Topic(topicName)
+	for i := 0; i < len(dataInResponse); i++ {
+		messageData, _ := json.Marshal(dataInResponse[i])
+		result := topic.Publish(ctx, &pubsub.Message{
+			Data: messageData,
+		})
+		_ = result
+		/*_, err := result.Get(ctx)
+		if err == nil {
+			messageCnt++
+		}*/
+		messageCnt++
+	}
+
+	fmt.Fprintf(w, "Published %v messages successfully\n", messageCnt)
 }
 
 func authCallback(w http.ResponseWriter, r *http.Request) {
